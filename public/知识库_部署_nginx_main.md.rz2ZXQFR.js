abba4bd98c3874eb35dddc7a0661c2f748ab4bca
pindemo <pindemo@users.noreply.github.com>
2024-09-13 02:26:33 +0000

    deploy: pindemo/vitepressblog@abe6825a76f28d87c42dd49ef58ba64eec2bd262

---
+++ public/知识库_部署_nginx_main.md.rz2ZXQFR.js
@@ -0,0 +1,5 @@
+import{_ as e,c as t,j as n,a as i,G as p,a4 as o,o as l,B as r}from"./chunks/framework.bh9iDcjP.js";const _=JSON.parse('{"title":"nginx","description":"","frontmatter":{},"headers":[],"relativePath":"知识库/部署/nginx/main.md","filePath":"知识库/部署/nginx/main.md","lastUpdated":1726194331000}'),c={name:"知识库/部署/nginx/main.md"};function d(m,a,g,x,h,u){const s=r("ArticleMetadata");return l(),t("div",null,[a[0]||(a[0]=n("h1",{id:"nginx",tabindex:"-1"},[i("nginx "),n("a",{class:"header-anchor",href:"#nginx","aria-label":'Permalink to "nginx"'},"​")],-1)),p(s),a[1]||(a[1]=o(`<div class="language- vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang"></span><pre class="shiki shiki-themes github-light github-dark vp-code" tabindex="0"><code><span class="line"><span>启动</span></span>
+<span class="line"><span>start nginx</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>重启</span></span>
+<span class="line"><span>nginx -s reload</span></span></code></pre></div>`,1))])}const v=e(c,[["render",d]]);export{_ as __pageData,v as default};
